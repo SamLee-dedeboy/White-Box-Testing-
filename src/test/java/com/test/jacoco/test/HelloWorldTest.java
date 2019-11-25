7d84cf6186d4f371382703e284d78b68c968bfba
sam <ydlee.nju@gmail.com>
2019-11-25 19:35:16 +0800

    finished

--- src/test/java/com/test/jacoco/test/HelloWorldTest.java
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -31,9 +31,51 @@ public class HelloWorldTest {
     	 */
     	HelloWorld hw = new HelloWorld();
     	int a = hw.Method3(6, -1, 1);
-    	
+    	int b = hw.Method3(4,-1,-3);
+        int c = hw.Method3(5,0,-1);
+        int d = hw.Method3(6,2,2);
+        int e = hw.Method3(4,-1,0);
     }
     /**
      * TODO: add the test function of other methods in HelloWorld.java
      */
+     @Test
+     public void testMethod4() {
+        HelloWorld hw = new HelloWorld();
+        int a = hw.Method4(0,0,1,2,1);
+        int b = hw.Method4(1,1,2,2,1);
+        int c = hw.Method4(5,5,2,2,1);
+        int d = hw.Method4(5,2,2,2,1);
+     }
+     @Test
+     public void testBug() {
+         HelloWorld hw = new HelloWorld();
+         boolean a = hw.bug(5);
+         boolean b = hw.bug(4);
+     }
+     @Test
+     public void testIsTriangle() {
+        HelloWorld hw = new HelloWorld();
+        boolean a = hw.isTriangle(1,2,3);
+        boolean b = hw.isTriangle(3,5,1);
+        boolean c = hw.isTriangle(5,1,4);
+        boolean d = hw.isTriangle(3,3,3);
+     }
+     @Test
+     public void testIsBirthday() {
+         HelloWorld hw = new HelloWorld();
+         boolean a = hw.isBirthday(1997,9,28);
+         boolean b = hw.isBirthday(2019,12,31);
+         boolean c = hw.isBirthday(100,1,1);
+     }
+     @Test
+     public void testMiniCalculator() {
+         HelloWorld hw = new HelloWorld();
+         double a = hw.miniCalculator(1,1,'+');
+         double b = hw.miniCalculator(1,1,'-');
+         double c = hw.miniCalculator(1,1,'*');
+         double d = hw.miniCalculator(1,1,'/');
+         double e = hw.miniCalculator(1,1,'0');
+
+     }
 }
